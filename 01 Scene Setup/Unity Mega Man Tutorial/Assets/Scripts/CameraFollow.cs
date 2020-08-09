1053795c1d8b7cc02966c8ca238473bd080fbd3b
tonybhimani <tonybhimani@users.noreply.github.com>
2020-08-09 13:12:15 -0700

    Add files via upload

---
+++ 01 Scene Setup/Unity Mega Man Tutorial/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] float timeOffset;
+    [SerializeField] Vector3 offsetPos;
+
+    [SerializeField] Vector3 boundsMin;
+    [SerializeField] Vector3 boundsMax;
+
+    private void LateUpdate()
+    {
+        if (player != null)
+        {
+            Vector3 startPos = transform.position;
+            Vector3 targetPos = player.position;
+
+            targetPos.x += offsetPos.x;
+            targetPos.y += offsetPos.y;
+            targetPos.z = transform.position.z;
+
+            targetPos.x = Mathf.Clamp(targetPos.x, boundsMin.x, boundsMax.x);
+            targetPos.y = Mathf.Clamp(targetPos.y, boundsMin.y, boundsMax.y);
+
+            float t = 1f - Mathf.Pow(1f - timeOffset, Time.deltaTime * 30);
+            transform.position = Vector3.Lerp(startPos, targetPos, t);
+        }
+    }
+}
---
+++ 01 Scene Setup/Unity Mega Man Tutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+
+    [SerializeField] float moveSpeed = 3f;
+    [SerializeField] float jumpSpeed = 5f;
+
+    float keyHorizontal;
+    bool keyJump;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        keyHorizontal = Input.GetAxisRaw("Horizontal");
+        keyJump = Input.GetKeyDown(KeyCode.Space);
+        rb2d.velocity = new Vector2(keyHorizontal * moveSpeed, rb2d.velocity.y);
+        if (keyJump)
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+        }
+    }
+}
