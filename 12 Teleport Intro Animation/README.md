4b25fe2ea3cafb998c07f07947a52a895a604803
tonybhimani <tonybhimani@users.noreply.github.com>
2020-11-07 20:11:53 -0800

    Add files via upload

---
+++ 12 Teleport Intro Animation/README.md
@@ -0,0 +1,7 @@
+# 12 Teleport Intro Animation
+
+Unity Mega Man Tutorial
+
+This is the project for Video 12 - Teleport Intro Animation
+
+Made with Unity 2019.3.10f1
---
+++ 12 Teleport Intro Animation/Unity Mega Man Tutorial/Assets/Scripts/AssetPalette.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssetPalette : MonoBehaviour
+{
+    public GameObject[] enemyPrefabs = new GameObject[4];
+
+    public enum EnemyList { BigEye, KillerBomb, Mambu, Pepe };
+    public EnemyList enemyList;
+}
