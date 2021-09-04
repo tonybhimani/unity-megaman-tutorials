d0ad77bc719ff5a8b68bc2fa455babe1553a9608
Tony Bhimani <tonybhimani@users.noreply.github.com>
2021-09-04 16:45:35 -0700

    Add files via upload

---
+++ 38 Ladders/README.md
@@ -0,0 +1,9 @@
+# 38 Ladders
+
+Unity Mega Man Tutorial
+
+This is the project for Video 38 - Ladders
+
+https://youtu.be/xoK0mPISY08
+
+Made with Unity 2019.3.10f1
---
+++ 38 Ladders/Unity Mega Man Tutorial/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public float timeOffset;
+    public Vector3 offsetPos;
+
+    public Vector3 boundsMin;
+    public Vector3 boundsMax;
+
+    private void LateUpdate()
+    {
+        if (player != null)
+        {
+            Vector3 startPos = transform.position;
+            Vector3 targetPos = player.position;
+
+            targetPos.x += offsetPos.x;
+            targetPos.y += offsetPos.y;
+            targetPos.z = transform.position.z;
+
+            targetPos.x = Mathf.Clamp(targetPos.x, boundsMin.x, boundsMax.x);
+            targetPos.y = Mathf.Clamp(targetPos.y, boundsMin.y, boundsMax.y);
+
+            float t = 1f - Mathf.Pow(1f - timeOffset, Time.deltaTime * 30);
+            transform.position = Vector3.Lerp(startPos, targetPos, t);
+        }
+    }
+}
